31609f70e876f48ae61bc36ab6472215f51fc538
Joselarrahondo <jlarraho@gmail.com>
2022-11-30 17:27:43 -0600

    create feature fixed

--- pets.js
+++ pets.js
@@ -15,30 +15,33 @@ if (subcommand === "read") {
       console.log(pets[petIndex]);
     }
   });
+
+  
 } else if ( subcommand === "create"){
     const age = process.argv[3];
     const name = process.argv[4];
-    const type = process.argv[3];
+    const type = process.argv[5];
     const pet = {age, name, type};
     readFile("./pets.json", "utf-8").then((text) => {
         const pets = JSON.parse(text);
         pets.push(pet);
        return writeFile("./pets.json", JSON.stringify(pets));
     });
+
+
+} else if (subcommand === "destroy"){
+    const petIndex = process.argv[3]
+    readFile("./pets.json", "utf-8").then((text) => {
+        const pets = JSON.parse(text);
+        if(petIndex !== undefined){
+            return delete(pets[petIndex])
+        }
+
+    })
+
+
 }else {
   console.error("Usage: node pets.js [read | create | update | destroy]");
   process.exit(1);
 }
 
-// const subTwo = process.argv[4];
-// if(subTwo === "create"){
-//     const newPet = process.argv[5]
-//     readFile("./pets.json", "utf-8").then((text) => {
-//         const newPet = JSON.parse(text);
-//         writeFile("./pets.json", "utf-8","name, age, kind").then((text) => {
-//             const addPet = JSON.parse(text);
-            
-        
-//         })
-// })
-// }
