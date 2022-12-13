5affea141d13b3b1fb926508fd586bcd15d28d9b
Joselarrahondo <jlarraho@gmail.com>
2022-12-13 15:30:11 -0600

    PART 2 COMPLETED, delete and patch

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -3,48 +3,39 @@ import { readFile, writeFile } from "node:fs/promises";
 import morgan from "morgan";
 import postgres from "postgres";
 
-const sql = postgres({database: "petshop"});
+const sql = postgres({database: "petshop",});
 
 const server = express();
 const port = 3001;
 
 server.use(express.json());
-
+server.use(morgan("tiny"))
 // accept get request to /pets, return pets and 200
 server.get("/pets", (req, res) => {
     sql`SELECT * FROM PETS`.then(pets => {
-        console.log("pets", pets);
         res.json(pets);
     })
-    // readFile("./pets.json", "utf-8").then((text) => {
-    //     res.setHeader("Content-type", "application/json");
-    //     res.json(JSON.parse(text));
-    // })
-})
+});
 
-// USING A GET REQUEST
-server.get("/pets/:index", (req, res, next) => {
-    const index = req.params.index;
-    readFile("./pets.json", "utf-8").then((text) => {
-        const pets = JSON.parse(text);
-        const selectedPet = pets[index];
-        if (index >= 0 && index <= pets.length -1) {
-            res.json(selectedPet);
-        } else {
+                                     ///////// USING A GET REQUEST/////////////
+
+server.get("/pets/:id", (req, res) => {
+    const id = req.params.id;
+    sql`SELECT * FROM pets WHERE id = ${id}`.then((result) => {
+        if(result.length === 0){
             res.set("Content-type", "text/plain");
             res.status(404);
             res.end("Not Found");
+        }else {
+            res.json(result[0]);
         }
-    })
-    .catch((error) => {
-        console.log("got error");
-        next(error);
     });
 });
-// USING A POST REQUEST
+                                           ////////// USING A POST REQUEST////////////
 server.post("/pets", (req, res, next) => {
     const pet = req.body;
-    const reqFields = [name, kind, age];
+    
+    const reqFields = ["name", "kind", "age"];
     const errors = [];
     for (let field of reqFields) {
         if (pet[field] === undefined) {
@@ -54,54 +45,64 @@ server.post("/pets", (req, res, next) => {
     if (pet.age && typeof pet.age !== "number") {
         errors.push("Pet age must be a number");
     }
+    
+    const {name, kind, age} = pet;
+    
     if (errors.length > 0) {
         res.status(422);
-        res.send(error.join(" "));
+        res.send(errors.join(" "));
 
     } else {
-        readFile("./pets.json", "utf-8").then((text) => {
-            const pets = JSON.parse(text);
-            pets.push(pet);
-            return writeFile("./pets.json", JSON.stringify(pets));
-        }).then(() => {
-            res.status(201);
-            res.send(JSON.stringify(pet));
-        }).catch(error => {
-            next(error);
-        })
+        sql `INSERT INTO pets (age, name, kind) VALUES (${age}, ${name}, ${kind}) RETURNING *`.then(
+            (result) => {
+                res.status(201);
+                res.json(result);
+         }
+        );  
     }
-})
-
-// USING A PATCH REQUEST
-server.patch("/pets/:index", (req, res) => {
-    const { index } = req.params;
-    const updates = req.body;
-    readFile("./pets.json", "utf-8").then((text) => {
-        const pets = JSON.parse(text);
-        // const existingPet = pets[index];
-        for (let key in updates) {
-            pets[index][key] = updates[key];
-        }
-        return writeFile("./pets.json", JSON.stringify(pets)).then(() => {
-            res.status(200);
-            res.send(pets);
-        })
-    })
 });
 
-// USING A DELETE REQUEST
+
+                                        ///////////// USING A PATCH REQUEST/////////
+
+server.patch("/pets/:id", async (req, res) => {
+    const id = req.params.id;
+    const pet = req.body;
+   
+    const {name, kind, age} = pet;
+    
+    if(pet.age !== undefined) {
+        sql `UPDATE pets SET age = ${age} WHERE id = ${id} RETURNING *`.then(
+            (result) =>{
+                res.status(201);
+                res.json(result[0]);
+            });
+    } 
+    if(pet.name !== undefined) {
+        sql `UPDATE pets SET name= ${name} WHERE id = ${id} RETURNING *`.then(
+            (result) =>{
+                res.status(201);
+                res.json(result[0]);
+            });
+    } 
+    if(pet.kind !== undefined) {
+        sql `UPDATE pets SET kind =${kind} WHERE id = ${id} RETURNING *`.then(
+            (result) =>{
+                res.status(201);
+                res.json(result[0]);
+            });
+    } 
+    });
+
+
+   
+                                            ////////////USING A DELETE REQUEST////////////
 server.delete("/pets/:index", (req, res) => {
-    const {index} = req.params;
-    readFile("./pets.json", "utf-8").then((text) => {
-        const pets = JSON.parse(text);
-        pets.splice(index, 1);
-        return writeFile("./pets.json", JSON.stringify(pets)).then(() => {
-            res.set("Content-type", "application/json");
-            res.status(200);
-            res.send(pets);
+        const {index} = req.params;
+        sql `DELETE FROM pets WHERE id = ${index} RETURNING *`.then((result) => {
+            res.send(result[0]);
         })
-    })
-})
+    });
 
 
 server.use((req, res) => {
@@ -114,4 +115,3 @@ server.listen(port, () => {
     console.log(`listening on ${port}`)
 });
 
- 
